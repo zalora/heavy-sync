a69072d29e5b890c81a5d1e5da3958024fa7471d
Hoang Xuan Phu <phuhimself@phunehehe.net>
2015-03-02 12:20:17 +0800

    make this pip-installable

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os.path import join
+
+setup(name='heavy-sync',
+      version='0.1',
+      description='Synchronize huge cloud buckets with ease',
+      author='Hoang Xuan Phu',
+      author_email='phu.hoang@zalora.com',
+      url='https://github.com/zalora/heavy-sync',
+      packages=['heavy-sync'],
+      install_requires=['boto'],
+      scripts=[join('heavy-sync', 'heavy-sync')],
+)
